32cc6e4b71e73df2d14b66101521847e9c41280c
Vitalii Lutsyshyn <Hooverbrian26@Gmail.com>
2026-04-20 19:14:11 +0200

    Add repository lookup for user tasks

--- backend/src/main/java/com/project/backend/Repository/TaskRepository.java
+++ backend/src/main/java/com/project/backend/Repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.project.backend.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.backend.Model.TaskModel;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+    Optional<TaskModel> findByIdAndUser_Id(Long id, Long userId);
 }
