557022804c16171498640b8290302f9dd3358edf
Anton Tsyns <tsynsanton@gmail.com>
2026-05-15 17:07:54 +0200

    fix: remove invalid task category mapping

--- backend/src/main/java/com/project/backend/Model/TaskCategoryModel.java
+++ backend/src/main/java/com/project/backend/Model/TaskCategoryModel.java
@@ -1,14 +1,10 @@
 package com.project.backend.Model;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,9 +17,6 @@ public class TaskCategoryModel {
 
     @Column(nullable = false, unique = true)
     private String name;
-
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<TaskModel> tasks;
 
     // Getters and Setters
     public Long getId() {
@@ -40,13 +33,5 @@ public class TaskCategoryModel {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<TaskModel> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<TaskModel> tasks) {
-        this.tasks = tasks;
     }
 }
